5d901990092c145ba4bc56b3231def932225f92c
ColinHask <colinhaskins2022@gmail.com>
2024-06-27 17:06:13 -0400

    changes

--- Funky_5_mins.py
+++ Funky_5_mins.py
@@ -1,13 +1,11 @@
 # Funky_5_mins
-import webbrowser
-import time
-import tkinter as tk
-from threading import Timer
 
 import webbrowser
 import time
 import pyautogui
-from threading import Timer
+import subprocess
+
+
 
 # URL of the YouTube video
 youtube_url = 'https://www.youtube.com/watch?v=poa_QBvtIBA'
@@ -17,15 +15,20 @@ def open_youtube_link():
     webbrowser.open(youtube_url)
     print(f"Opened YouTube link: {youtube_url}")
 
-# Function to show the popup message
-def show_popup():
-    pyautogui.alert(text='Get funky!', title='Reminder', button='OK')
-    time.sleep(10)  # Wait for 10 seconds before opening the link
-    open_youtube_link()
+def message():
+    # Open Notepad
+    subprocess.Popen('notepad.exe')
+
+    # Wait for Notepad to open
+    time.sleep(2)
+
+    # Type "get funky"
+    pyautogui.write('get funky', interval=0.1)
 
 # Main loop to open the link every 5 minutes
 while True:
-    # Show the popup 10 seconds before opening the link
-    Timer(290, show_popup).start()
-    # Wait for 5 minutes (300 seconds) before showing the next popup
-    time.sleep(300)
+    time.sleep(150)
+    message()
+    time.sleep(3)
+    open_youtube_link()
+    
